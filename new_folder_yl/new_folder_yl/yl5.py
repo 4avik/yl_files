765d31c849bef0842a2d25d4e372434e08b1dbee
4avik <91154227+4avik@users.noreply.github.com>
2021-11-15 12:49:57 +0200

    Update yl5.py

--- new_folder_yl/new_folder_yl/yl5.py
+++ new_folder_yl/new_folder_yl/yl5.py
@@ -10,7 +10,7 @@
 
 
 #alamavaldis			        alamavaldised						
-#B OR C | A AND (B OR C)|	A ~ B	|C AND A |	NOT(C AND A) |	(A ~ B) OR NOT(C AND A)|			
+#B OR C | A AND (B OR C)    |	A ~ B       |C AND A     |   NOT(C AND A)    |	(A ~ B) OR NOT(C AND A)            |			
 #  0    |     0		    |     1	    |   0	 |      1	     |           1			   | 
 #  1    |     0		    |     1	    |   0	 |      1	     |           1			   | 
 #  1    |     0		    |     0	    |   0	 |      1	     |           1			   | 
